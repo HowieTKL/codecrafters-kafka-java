ef9f0a598872caf51f0739084d432f5ea84e1bff
HowieTKL <10776992+HowieTKL@users.noreply.github.com>
2025-02-26 11:32:49 -0500

    Parse and send back correlation id

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,11 @@
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args){
+  public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.err.println("Logs from your program will appear here!");
 
@@ -12,25 +13,34 @@ public class Main {
     Socket clientSocket = null;
     int port = 9092;
     try {
-     serverSocket = new ServerSocket(port);
-     // Since the tester restarts your program quite often, setting SO_REUSEADDR
-     // ensures that we don't run into 'Address already in use' errors
-     serverSocket.setReuseAddress(true);
-     // Wait for connection from client.
-     clientSocket = serverSocket.accept();
-     OutputStream out = clientSocket.getOutputStream();
-     out.write(new byte[] {0, 0, 0, 0});
-     out.write(new byte[] {0, 0, 0, 7});
+      serverSocket = new ServerSocket(port);
+      // Since the tester restarts your program quite often, setting SO_REUSEADDR
+      // ensures that we don't run into 'Address already in use' errors
+      serverSocket.setReuseAddress(true);
+      // Wait for connection from client.
+      clientSocket = serverSocket.accept();
+      InputStream in = clientSocket.getInputStream();
+      byte[] messageSize = new byte[4];
+      byte[] requestApiKey = new byte[2];
+      byte[] requestApiVersion = new byte[2];
+      byte[] correlationId = new byte[4];
+      in.read(messageSize);
+      in.read(requestApiKey);
+      in.read(requestApiVersion);
+      in.read(correlationId);
+      OutputStream out = clientSocket.getOutputStream();
+      out.write(new byte[]{0, 0, 0, 0});
+      out.write(correlationId);
     } catch (IOException e) {
-     System.out.println("IOException: " + e.getMessage());
+      System.out.println("IOException: " + e.getMessage());
     } finally {
-     try {
-       if (clientSocket != null) {
-         clientSocket.close();
-       }
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
+      try {
+        if (clientSocket != null) {
+          clientSocket.close();
+        }
+      } catch (IOException e) {
+        System.out.println("IOException: " + e.getMessage());
+      }
     }
   }
 }
